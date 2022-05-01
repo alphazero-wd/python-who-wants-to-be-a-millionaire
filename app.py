c073b521c0d816a58452d5dc0ff7b1c3ebca54c5
alphazero-wd <alphazero25811@gmail.com>
2022-05-01 20:11:25 +0700

    update questions

--- app.py
+++ app.py
@@ -6,85 +6,281 @@
 import random
 from typing import List
 
-
 class Question:
-  def __init__(self, question, choices, correct_ans):
+  def __init__(self, question, choices, correct_ans, difficulty):
     self.question: str = question
-    self.choices: List[int] = choices
+    self.choices: List[str] = choices
     self.correct_ans: str = correct_ans
+    self.difficulty = difficulty
   
+money_earned = 0
 question_choices_keys = ['A', 'B', 'C', 'D']
-valid_inputs = ['a', 'b', 'c', 'd', 'leave']
-prizes = [100, 200, 300, 500, 1000, 2000, 4000, 8000, 16000, 32000, 64000, 128000, 250000, 500000, 1000000]
-questions: List[Question] = [Question('In the UK, the abbreviation NHS stands for National what Service?',  ['Humanity', 'Health', 'Honour', 'Household'], 'Health'),Question('Which Disney character famously leaves a glass slipper behind at a royal ball?',  ['Pocahontas', 'Sleeping Beauty', 'Cinderella', 'Elsa'],  'Cinderella'),Question(  'What name is given to the revolving belt machinery in an airport that delivers checked luggage from the plane to baggage reclaim?',  ['Hangar', 'Terminal', 'Concourse', 'Carousel'],  'Carousel'),Question('Which of these brands was chiefly associated with the manufacture of household locks?',  ['Phillips', 'Flymo', 'Chubb', 'Ronseal'],  'Chubb'),Question(  'The hammer and sickle is one of the most recognisable symbols of which political ideology?',  ['Republicanism', 'Communism', 'Conservatism', 'Liberalism'],  'Communism'),Question(  'Which toys have been marketed with the phrase “robots in disguise”?',  ['Bratz Dolls', 'Sylvanian Families', 'Hatchimals', 'Transformers'],  'Transformers'),Question(  'What does the word loquacious mean?',  ['Angry', 'Chatty', 'Beautiful', 'Shy'],  'Chatty'),Question(  'Obstetrics is a branch of medicine particularly concerned with what?',  ['Childbirth', 'Broken homes', 'Heart conditions', 'Old age'],  'Childbirth'),Question(  'In Doctor Who, what was the signature look of the fourth Doctor, as portrayed by Tom Baker?',  [    'Bow-tie, braces and tweed jacket',    'Wide-brimmed hat and extra long scarf',     'Pinstripe suit and trainers',     'Cape, velvet jacket and frilly shirt'  ],  'Wide-brimmed hat and extra long scarf'),Question(  'Which of these religious observances lasts for the shortest period of time during the calendar year?',  [    'Ramadan',     'Diwali',     'Lent',     'Hanukkah'  ],  'Diwali'),Question(  'At the closest point, which island group is only 50 miles south-east of the coast of Florida?',  ['Bahamas', 'US Virgin Islands', 'Turks and Caicos Islands', 'Bermuda'],  'Bahamas'),Question(  'Construction of which of these famous landmarks was completed first?',  ['Empire State Building', 'Royal Albert Hall', 'Effiel Tower', 'Big Ben Clock Tower'],  'Big Ben Clock Tower'),Question(  'Which of these cetaceans is classified as a “toothed whale”?',  ['Gray whale', 'Minke whale', 'Sperm whale', 'Humpback whale'],  'Sperm whale'),Question(  'Who is the only British politician to have held all four “Great Offices of State” at some point during their career?',  ['David Lloyd George', 'Harold Wilson', 'James Callaghan', 'John Major'],  'James Callaghan'),Question(  'In 1718, which pirate died in battle off the coast of what is now North Carolina?',  ['Calico Jack', 'Blackbeard', 'Bartholomew Roberts', 'Captain Kidd'],  'Blackbeard')]
-
-# used to shuffle questions and choices
-def shuffle(_):
-  return 0.5 - random.random()
+valid_inputs = ['a', 'b', 'c', 'd', 'q', 'h']
+fifty_fifty_left = 1
+bounty = 100
+questions: List[Question] = [
+  Question(
+    "Which of the following is the correct extension of the Javascript file?",
+    ['.js', '.ts', '.🐍', '.bf'],
+    '.js',
+    'easy'
+  ),
+  Question(
+    'Which line of JavaScript code includes the use of the ternary operator?',
+    [
+      'var answer = 100 φ 26;',
+      'var answer = (age >= 16) ? "Drive" : "Walk";',
+      'var answer = "Drive" || "Walk";',
+      'var answer = (age >⎶ 16) ‽ "Drive" ܊ "Walk";'
+    ],
+    'var answer = (age >= 16) ? "Drive" : "Walk";',
+    'easy'
+  ),
+  Question(
+    'In HTML, how do you create an h2 element that says "CatPhotoApp"?',
+    [
+      '<header2>CatPhotoApp</header2>',
+      '<h2>CatPhotoApp<h2>',
+      'h2 = "CatPhotoApp"',
+      '<h2>CatPhotoApp</h2>',
+    ],
+    '<h2>CatPhotoApp</h2>',
+    'easy'
+  ),
+  Question(
+    'What is the correct way to create a function in Javascript?',
+    [
+      'def myFunction():',
+      'function myFunction():',
+      'function myFunction() { }',
+      'fn myFunction() end',
+    ],
+    'function myFunction() { }',
+    'easy'
+  ),
+  Question(
+    'Which symbols are used for a comment in JavaScript?',
+    [
+      '//',
+      '\\\\',
+      '\* *\ ',
+      '¯\_(ツ)_/¯',
+    ],
+    '//',
+    'easy'
+  ),
+  Question(
+    'Of the following JavaScript if statements, which one correctly executes three instructions if the condition is true?',
+    [
+      'if (x < 0) a = b * 2; y = x; z = a – y;',
+      'if{ (x < 0) a = b * 2; y = x; z = a – y ; }',
+      'if (x < 0) { a = b * 2; y = x; z = a – y; }',
+      'if (x < 0) { y = x; z = a – y * x + 1; }',
+    ],
+    'if (x < 0) { a = b * 2; y = x; z = a – y; }',
+    'easy'
+  ),
+  Question(
+    '''
+    What is the output of the following Javascript code?
+    console.log('3' + 4 + 5) 
+    ''',
+    ['345', 'TypeError: must be string, not number', '12', '39'],
+    '345',
+    'medium'
+  ),
+  Question(
+    '''
+    What is the output of the following Javascript code?
+    var a = ['dog', 'cat', 'hen'];
+    a[100] = 'fox';
+    console.log(a.length); 
+    ''',
+    ['101', 'IndexError: list assignment index out of range', '4', 'undefined'],
+    '101',
+    'medium'
+  ),
+  Question(
+    '''
+    What is the output of the following Javascript code?
+    var a = 10;
+    console.log(a++)
+    ''',
+    ['10', 'NameError: name \'a\' is not defined', '11', 'undefined'],
+    '10',
+    'medium'
+  ),
+  Question(
+    '''
+    What is the output of the following Javascript code?
+    function makePerson(first, last) {
+      return {
+        first: first,
+        last: last,
+        fullName: function() {
+          return this.first + ' ' + this.last;
+        }
+      };
+    }
+    var s = makePerson('Simon', 'Willison');
+    var fullName = s.fullName;
+    console.log(fullName());
+    ''',
+    ['undefined undefined', 'Simon Willison', 'Willison Simon', 'null null'],
+    'undefined undefined',
+    'hard'
+  ),
+  Question(
+    '''
+    What is the output of the following Javascript code?
+    function Person(name, age) {
+      this.name = name
+      this.age = age
+    }
+    var person1 = new Person('John', 30)
+    var person2 = person1
+    person2.age = 28
+    console.log(person1.age)
+    ''',
+    ['28', '30', 'John', "undefined"],
+    '28',
+    'hard'
+  ),
+  Question(
+  'Which of the following code returns the maximum number in an array in Javascript?',
+  [
+    'Math.max(array)',
+    'Math.max(*array)',
+    'Math.max.call(null, array)',
+    'Math.max.apply(null, array)'
+  ],
+  'Math.max.apply(null, array)',
+  'hard'
+  ),
+  Question(
+    '''
+    What is the output of the following Javascript code?
+    function Person(name, age) {
+      this.name = name
+      this.age = age
+    }
+    var person1 = Person('John', 28)
+    console.log(person1.name)
+    ''',
+    [
+      'undefined', 
+      'null', 
+      'John', 
+      'TypeError: can\'t access property \"name\" of \"person1\"'
+    ],
+    'undefined',
+    'extreme'
+  ),
+  Question(
+    '''
+    What is the output of the following Javascript code?
+    'use strict';
+    function fun() { return this; }
+    console.log(!fun() == 1); 
+    ''',
+    ['true', 'false', 'undefined', 'null'],
+    'true',
+    'extreme'
+  ),
+  Question(
+    '''
+    What is the output of the following Javascript code?
+    var a = [-4, -7, 8, -9, 10, 20, -3, 2]
+    a.map(function (num) {
+      return -num
+    })
+    .sort(function(a, b) {
+      return b - a
+    })
+    console.log(a)
+    ''',
+    [
+      '[4, 7, -8, 9, -10, -20, 3, -2]',
+      '[-4, -7, 8, -9, 10, 20, -3, 2]',
+      '[20, 10, 8, 2, -3, -4, -7, -9]',
+      '[9, 7, 4, 3, -2, -8, -10, -20]'
+    ],
+    '[-4, -7, 8, -9, 10, 20, -3, 2]',
+    'extreme'
+  )
+]
 
 # format the money earned
-def format_number(number):
-  return "{:,}".format(number)
-
-def display_money_earned(money_earned):
-  return f'Money Earned: ${format_number(money_earned)}\n'
+def display_money_earned():
+  global money_earned
+  def format_number():
+    return "{:,}".format(money_earned)
+  print(f'Money Earned: ${format_number()}\n')
 
 def display_questions():
+    global money_earned
+    global is_game_over
     is_game_over = False
-    money_earned = 0
-    questions.sort(key=shuffle)
     for i, question in enumerate(questions):
       if is_game_over:
         break
-      print(f'Question {i + 1}: {question.question}')
-      question.choices.sort(key=shuffle)
+      print(f'Question {i + 1} ({question.difficulty}): {question.question}')
+      question.choices.sort(key=lambda _: 0.5 - random.random())
 
       # loop through the choices and shuffle them
       for index, choice in enumerate(question.choices):
         print(f'{question_choices_keys[index]}. {choice}')
 
-      # check user's choice
-      player_choice = input('Type in your choice [A/B/C/D] (case-insensitive) or type in leave to leave the game: ')
+      check_ans(i, question)
+
+def check_ans(i, question: Question):
+    global money_earned
+    global is_game_over
+    # check user's choice
+    player_choice = input('Type in your choice [A/B/C/D], Q to leave the game or H to use 50/50 (case-insensitive): ')
+
+    # if user enters invalid input
+    while player_choice not in valid_inputs:
+      player_choice = input("That's not a valid choice. Please try again: ")
+
+    correct_ans_index = question.choices.index(question.correct_ans)
 
       # if user leaves, preserve their prize
-      if player_choice.lower() == 'leave':
-        print('It\'s a safe choice to leave the game if you\'re uncertain of your answer. Good choice :)')
-        print(display_money_earned(money_earned))
+    if player_choice.lower() == 'q':
+      print('It\'s a safe choice to leave the game if you\'re uncertain of your answer. Good choice :)')
+      display_money_earned()
+      is_game_over = True
+    
+    # check user's ans to the correct 
+    player_ans_index = question_choices_keys.index(player_choice.upper())
+    player_ans = question.choices[player_ans_index]
+
+    # if their's answer is correct, add money and move on to the next question
+    if question.correct_ans == player_ans:
+      global bounty
+      money_earned += bounty
+      bounty *= 2
+      if i == len(questions) - 1:
+        print(f'You\'ve become a millionaire. Good luck in your next game :)')
+        display_money_earned()
         is_game_over = True
-
-      # if user enters invalid input
-      while player_choice not in valid_inputs:
-        player_choice = input("That's not a valid choice. Please try again: ")
-
-      # check user's ans to the correct 
-      player_ans_choice = question.choices[question_choices_keys.index(player_choice.upper())]
-      correct_ans_choice = question_choices_keys[question.choices.index(question.correct_ans)]
-
-      # if their's answer is correct, add money and move on to the next question
-      if question.correct_ans == player_ans_choice:
-        money_earned += prizes[i]
-        if i == len(questions) - 1:
-          print(f'You\'ve become a millionaire. Good luck in your next game :)')
-          print(display_money_earned(money_earned))
-          is_game_over = True
-        else:
-          print(f'\nYou are correct. Let\'s move on to the next question.')
-          print(display_money_earned(money_earned))
-      
-      # if their's answer is incorrect, they only get $1000 and leave
       else:
-        if money_earned > 1000:
-          money_earned = 1000
-        print(f'Oops, the correct answer is {correct_ans_choice}. {question.correct_ans}. Try again :(')
-        print(display_money_earned(money_earned))
-        is_game_over = True
-
+        print(f'\nYou are correct. Let\'s move on to the next question.')
+        display_money_earned()
+      
+    # if their's answer is incorrect, they only get $1000 and leave
+    else:
+      if money_earned > 1000:
+        money_earned = 1000
+      print(f'Oops, the correct answer is {question_choices_keys[correct_ans_index]}. {question.correct_ans} :(')
+      display_money_earned()
+      is_game_over = True
 
 def start():
   print('Welcome to Who Wants to be a Millionaire.')
   print('To win the game, you have to overcome 15 questions.')
   print('You will earn an amount of money as you answer a question correctly.')
-  isConfirmed = input('If you answer all 15 questions correctly by typing your answer, you are a "millionaire". Are you ready to play? [Y/N]: ')
+  isConfirmed = input('If you answer all 15 questions correctly by typing your answer, you are a "millionaire". Are you ready to play? [y/n]: ')
   if isConfirmed.lower() == 'y':
     display_questions()
   else: exit()
